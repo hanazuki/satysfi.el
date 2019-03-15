4e4d8a4d1e80b3425c69c90bfb05e447eb34f2d3
Kasumi Hanazuki <kasumi@rollingapple.net>
2019-03-15 03:25:58 +0000

    highlight string literals

--- satysfi-mode.el
+++ satysfi-mode.el
@@ -20,28 +20,47 @@
     st)
   "Syntax table for `satysfi-mode'.")
 
-(defconst satysfi-syntax-propertize-rules
-  '(((rx (group (any "-|") ?>)) ;; TODO: handling of > should be context-aware
-     (1 "."))
-    ((rx (group ?') (group ?<))
-     (1 "'")
-     (2 "(>"))
-    ((rx (group ?#) (1+ ?`))
-     (1 "|"))
-    ((rx (group ?`) (0+ ?`))
-     (1 "|"))))
+(eval-when-compile
+  (defconst satysfi-syntax-propertize-rules
+    '(((rx (group (any "-|") ?>)) ;; TODO: handling of > should be context-aware
+       (1 "."))
+      ((rx (group ?') (group ?<))
+       (1 "'")
+       (2 "(>"))
+      ((rx (group ?$) ?{)
+       (1 "'")))))
 
 (defun satysfi-syntax-propertize (beg end)
-;;  (while (< beg end)
-    (let ((ppss (syntax-ppss beg)))
-;;      (if (eq (nth 3 ppss) t) ;; if this is inside ` string
-;;          (satysfi--syntax-propertize-string-literal ppss beg end)
-        (funcall (syntax-propertize-rules satysfi-syntax-propertize-rules)
-                 beg end)));;))
+  (funcall (syntax-propertize-rules satysfi-syntax-propertize-rules)
+           beg end)
 
-(defun satysfi--syntax-propertize-string-literal (ppss beg end)
-  )
+  (catch 'exit
+    (save-excursion
+      (goto-char beg)
+      (while (< (point) end)
+        (let ((ppss (syntax-ppss)))
+          (if (eq (nth 3 ppss) t) ;; if this is inside ` string
+              (let ((open-marker
+                     (save-excursion
+                       (goto-char (nth 8 ppss))
+                       (looking-at (rx (? ?#) (group (+ ?`))))
+                       (match-string 1))))
+                (unless (search-forward open-marker end t)
+                  (throw 'exit t))
+                (unless (looking-at-p "#")
+                  (forward-char -1))
+                (put-text-property (point) (1+ (point)) 'syntax-table (string-to-syntax "|"))
+                (forward-char +1))
 
+            (forward-comment (buffer-size))
+            (unless (re-search-forward (rx (any "#`")) end t)
+              (throw 'exit t))
+            (goto-char (match-beginning 0))
+
+            (cond ((looking-at (rx (? ?#) (+ ?`)))
+                   (put-text-property (point) (1+ (point)) 'syntax-table (string-to-syntax "|"))
+                   (goto-char (match-end 0)))
+                  (t (forward-char 1)))))))))
 
 (defconst satysfi-mode-program-keywords-regexp
    (regexp-opt '("let" "let-rec" "let-mutable" "let-inline" "let-block" "let-math" "in" "and"
@@ -70,6 +89,7 @@
   (set-syntax-table satysfi-mode-syntax-table)
   (setq-local syntax-propertize-function #'satysfi-syntax-propertize)
   (setq-local parse-sexp-lookup-properties t)
+  (setq-local font-lock-multiline t)
   (setq-local font-lock-defaults '(satysfi-mode-font-lock-keywords nil nil))
   (run-mode-hooks 'satysfi-mode-hook))
 
