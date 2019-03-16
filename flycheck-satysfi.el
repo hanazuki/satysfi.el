fd3c3e7f8b5f9156f96ae76caec4110808b85d14
Kasumi Hanazuki <kasumi@rollingapple.net>
2019-03-16 03:21:09 +0000

    license under GPL3+

--- flycheck-satysfi.el
+++ flycheck-satysfi.el
@@ -1,3 +1,27 @@
+;;; flycheck-satysfi.el --- Flycheck checker definition for SATySFi  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Kasumi Hanazuki
+
+;; Author: Kasumi Hanazuki <kasumi@rollingapple.net>
+;; Package-Requires: (flycheck)
+;; Keywords: satysfi, flycheck
+;; URL: https://github.com/hanazuki/satysfi.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (require 'flycheck)
 
 (defvar flycheck-satysfi-typecheck-only
@@ -23,3 +47,5 @@
   (add-to-list 'flycheck-checkers 'satysfi))
 
 (provide 'flycheck-satysfi)
+
+;;; flycheck-satysfi.el ends here
--- satysfi-mode.el
+++ satysfi-mode.el
@@ -1,4 +1,25 @@
-;;; satysfi-mode.el -- major mode for editing SATySFi documents  -*- lexical-binding: t; -*-
+;;; satysfi-mode.el --- major mode for editing SATySFi documents  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Kasumi Hanazuki
+
+;; Author: Kasumi Hanazuki <kasumi@rollingapple.net>
+;; Keywords: satysfi, languages
+;; URL: https://github.com/hanazuki/satysfi.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (require 'rx)
 (require 'seq)
@@ -278,3 +299,5 @@
   (run-mode-hooks 'satysfi-mode-hook))
 
 (provide 'satysfi-mode)
+
+;;; satysfi-mode.el ends here
